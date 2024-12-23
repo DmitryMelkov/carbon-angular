68c6e10fb8c11e9430d24bd6398da40e1532b919
Dmitry <melkov.dmitriy40@gmail.com>
2024-12-23 14:40:34 +0500

    add styles for home page

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -3,23 +3,22 @@ import { CommonModule } from '@angular/common';
 import { SushilkaComponent } from '../sushilki/sushilka-current/sushilka.component';
 import { SushilkaMnemoComponent } from '../sushilki/sushilka-mnemo/sushilka-mnemo.component';
 import { MatTabsModule } from '@angular/material/tabs';
+import { ControlButtonComponent } from '../../components/control-button/control-button.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule, SushilkaComponent, SushilkaMnemoComponent, MatTabsModule],
+  imports: [CommonModule, SushilkaComponent, SushilkaMnemoComponent, MatTabsModule, ControlButtonComponent],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
-  sushilkaIds = ['sushilka1', 'sushilka2']; // Массив с ID сушилок
-  selectedSushilkaId: string = this.sushilkaIds[0]; // Выбранная сушилка по умолчанию
+  objectData = [
+    { id: 'sushilka1', name: 'Сушилка №1' },
+    { id: 'sushilka2', name: 'Сушилка №2' },
+  ];
+  selectedSushilkaId: string = this.objectData[0].id; // Выбранный объект по умолчанию
   activeView: 'parameters' | 'mnemo' = 'parameters'; // Текущее отображаемое представление
-
-  // Метод для обработки завершения загрузки
-  onLoadingComplete() {
-    // Здесь вы можете добавить логику, которая будет выполняться после загрузки данных
-  }
 
   // Метод для отображения текущих параметров
   showParameters(id: string) {
