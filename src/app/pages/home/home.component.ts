15e4e88c05197b7b0026eba40bfe48c70340262a
Dmitry <melkdim@list.ru>
2025-01-25 17:51:56 +0500

    add home page fix

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,18 +1,14 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatTabsModule } from '@angular/material/tabs';
+import { ControlButtonComponent } from '../../components/control-button/control-button.component';
 import { SushilkaComponent } from '../sushilki/sushilka-current/sushilka.component';
 import { SushilkaMnemoComponent } from '../sushilki/sushilka-mnemo/sushilka-mnemo.component';
 import { MpaComponent } from '../mpa/mpa-current/mpa.component';
 import { MpaMnemoComponent } from '../mpa/mpa-mnemo/mpa-mnemo.component';
-import { MatTabsModule } from '@angular/material/tabs';
-import { ControlButtonComponent } from '../../components/control-button/control-button.component';
+import { VrComponent } from '../vr/vr-current/vr.component';
 import { GraphicVacuumsGeneralComponent } from '../sushilki/sushilka-graph-general/graphic-vacuums-general/graphic-vacuums-general.component';
 import { GraphicTempersGeneralComponent } from '../sushilki/sushilka-graph-general/graphic-tempers-general/graphic-tempers-general.component';
-import { EnergyResourcesCurrentComponent } from '../energy-resources/energy-resources-current/energy-resources-current.component';
-import { EnergyResourcesReportDayComponent } from '../energy-resources/energy-resources-report-day/energy-resources-report-day.component';
-import { EnergyResourcesReportMonthComponent } from '../energy-resources/energy-resources-report-month/energy-resources-report-month.component';
-import { EnergyResourcesGraphPressureComponent } from '../energy-resources/energy-resources-graph-pressure/energy-resources-graph-pressure.component';
-import { EnergyResourcesGraphConsumptionComponent } from '../energy-resources/energy-resources-graph-consumption/energy-resources-graph-consumption.component';
 import { GraphicMpaGeneralComponent } from '../mpa/mpa-graph-general/mpa-graph-general.component';
 import { MillsCurrentComponent } from '../mills/mills-current/mills-current.component';
 import { Mill1GraphComponent } from '../mills/mill1-graph/mill1-graph.component';
@@ -23,27 +19,26 @@ import { MillYCVOK130Component } from '../mills/mill-ycvok130/mill-ycvok130.comp
 import { ReactorComponent } from '../reactors/reactors-current/reactors.component';
 import { ReactorMnemoComponent } from '../reactors/reactors-mnemo/reactors-mnemo.component';
 import { GraphicReactorsGeneralComponent } from '../reactors/reactors-graph-general/reactors-graph-general.component';
-import { VrComponent } from '../vr/vr-current/vr.component';
+import { EnergyResourcesCurrentComponent } from '../energy-resources/energy-resources-current/energy-resources-current.component';
+import { EnergyResourcesReportDayComponent } from '../energy-resources/energy-resources-report-day/energy-resources-report-day.component';
+import { EnergyResourcesReportMonthComponent } from '../energy-resources/energy-resources-report-month/energy-resources-report-month.component';
+import { EnergyResourcesGraphPressureComponent } from '../energy-resources/energy-resources-graph-pressure/energy-resources-graph-pressure.component';
+import { EnergyResourcesGraphConsumptionComponent } from '../energy-resources/energy-resources-graph-consumption/energy-resources-graph-consumption.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [
     CommonModule,
+    MatTabsModule,
+    ControlButtonComponent,
     SushilkaComponent,
     SushilkaMnemoComponent,
     MpaComponent,
     MpaMnemoComponent,
     VrComponent,
-    MatTabsModule,
-    ControlButtonComponent,
     GraphicVacuumsGeneralComponent,
     GraphicTempersGeneralComponent,
-    EnergyResourcesCurrentComponent,
-    EnergyResourcesReportDayComponent,
-    EnergyResourcesReportMonthComponent,
-    EnergyResourcesGraphPressureComponent,
-    EnergyResourcesGraphConsumptionComponent,
     GraphicMpaGeneralComponent,
     MillsCurrentComponent,
     Mill1GraphComponent,
@@ -54,6 +49,11 @@ import { VrComponent } from '../vr/vr-current/vr.component';
     ReactorComponent,
     ReactorMnemoComponent,
     GraphicReactorsGeneralComponent,
+    EnergyResourcesCurrentComponent,
+    EnergyResourcesReportDayComponent,
+    EnergyResourcesReportMonthComponent,
+    EnergyResourcesGraphPressureComponent,
+    EnergyResourcesGraphConsumptionComponent,
   ],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
@@ -67,137 +67,90 @@ export class HomeComponent {
     { id: 'sushilka1', name: 'Сушилка №1', type: 'sushilka' },
     { id: 'sushilka2', name: 'Сушилка №2', type: 'sushilka' },
   ];
-  selectedObjectId: string = this.objectData[0].id; // Выбранный объект по умолчанию
-  activeView:
-    | 'parameters'
-    | 'mnemo'
-    | 'graph-vacuums-general'
-    | 'graph-tempers-general'
-    | 'graph-mpa-general'
-    | 'energy-resources'
-    | 'daily-report'
-    | 'monthly-report'
-    | 'reactors-current'
-    | 'reactors-mnemo'
-    | 'graph-reactors-general'
-    | 'energy-resources-graph-pressure'
-    | 'energy-resources-graph-consumption'
-    | 'mills-current'
-    | 'mill1-graph'
-    | 'mill2-graph'
-    | 'millsbm3-graph'
-    | 'millygm9517-graph'
-    | 'millycvok130-graph' = 'parameters';
 
-  // Метод для отображения текущих параметров
-  showParameters(id: string) {
-    this.selectedObjectId = id;
-    this.activeView = 'parameters';
-  }
+  selectedObjectId: string = this.objectData[0].id;
+  activeView: string = 'parameters';
 
-  // Метод для отображения мнемосхемы
-  showMnemo(id: string) {
-    this.selectedObjectId = id;
-    this.activeView = 'mnemo';
-  }
-
-  showGraphMpaGeneral(objectId: string) {
-    this.activeView = 'graph-mpa-general';
+  // Общий метод для изменения состояния
+  setView(view: string, objectId: string = '') {
+    this.activeView = view;
     this.selectedObjectId = objectId;
   }
 
-  // Метод для отображения графиков давления
+  // Методы для отображения различных видов контента
+  showParameters(id: string) {
+    this.setView('parameters', id);
+  }
+
+  showMnemo(id: string) {
+    this.setView('mnemo', id);
+  }
+
+  showGraphMpaGeneral(id: string) {
+    this.setView('graph-mpa-general', id);
+  }
+
   showGraphPressure(id: string) {
-    this.selectedObjectId = id;
-    this.activeView = 'graph-vacuums-general';
+    this.setView('graph-vacuums-general', id);
   }
 
-  // Метод для отображения графиков температур
   showGraphTemper(id: string) {
-    this.selectedObjectId = id;
-    this.activeView = 'graph-tempers-general';
+    this.setView('graph-tempers-general', id);
   }
 
-  // Метод для отображения текущих реакторов
   showReactorsCurrent() {
-    this.selectedObjectId = '';
-    this.activeView = 'reactors-current';
+    this.setView('reactors-current');
   }
 
-    // Метод для отображения текущих реакторов
-    showReactorsMnemo() {
-      this.selectedObjectId = '';
-      this.activeView = 'reactors-mnemo';
-    }
+  showReactorsMnemo() {
+    this.setView('reactors-mnemo');
+  }
 
-  // Метод для отображения графиков реакторов
   showReactorsGeneral() {
-    this.selectedObjectId = '';
-    this.activeView = 'graph-reactors-general';
+    this.setView('graph-reactors-general');
   }
 
-  // Метод для отображения энергоресурсов
   showEnergyResources() {
-    this.selectedObjectId = ''; // Сбросьте идентификатор, так как это не сушилка
-    this.activeView = 'energy-resources';
+    this.setView('energy-resources');
   }
 
-  // Метод для отображения суточного отчета
   showDailyReport() {
-    this.selectedObjectId = ''; // Сбросьте идентификатор, так как это не сушилка
-    this.activeView = 'daily-report';
+    this.setView('daily-report');
   }
 
-  // Метод для отображения месячного отчета
   showMonthlyReport() {
-    this.selectedObjectId = ''; // Сбросьте идентификатор, так как это не сушилка
-    this.activeView = 'monthly-report';
+    this.setView('monthly-report');
   }
 
-  // Метод для отображения графиков давления в энергоресурсах
   showEnergyResourcesGraphPressure() {
-    this.selectedObjectId = ''; // Сбросьте идентификатор, так как это не сушилка
-    this.activeView = 'energy-resources-graph-pressure';
+    this.setView('energy-resources-graph-pressure');
   }
 
   showEnergyResourcesGraphConsumption() {
-    this.selectedObjectId = ''; // Сбросьте идентификатор, так как это не сушилка
-    this.activeView = 'energy-resources-graph-consumption';
+    this.setView('energy-resources-graph-consumption');
   }
 
-  // Метод для отображения текущих параметров мельниц
   showMillsCurrent() {
-    this.selectedObjectId = ''; // Сбросьте идентификатор, так как это не сушилка
-    this.activeView = 'mills-current';
+    this.setView('mills-current');
   }
 
-  // Метод для отображения графиков мельницы 1
   showMill1Graph() {
-    this.selectedObjectId = ''; // Сбросьте идентификатор, так как это не сушилка
-    this.activeView = 'mill1-graph';
+    this.setView('mill1-graph');
   }
 
-  // Метод для отображения графиков мельницы 2
   showMill2Graph() {
-    this.selectedObjectId = ''; // Сбросьте идентификатор, так как это не сушилка
-    this.activeView = 'mill2-graph';
+    this.setView('mill2-graph');
   }
 
-  // Метод для отображения графиков мельницы ШБМ №3
   showMillsbm3Graph() {
-    this.selectedObjectId = ''; // Сбросьте идентификатор, так как это не сушилка
-    this.activeView = 'millsbm3-graph';
+    this.setView('millsbm3-graph');
   }
 
-  // Метод для отображения графиков мельницы ШБМ №3
   showMillygm9517Graph() {
-    this.selectedObjectId = ''; // Сбросьте идентификатор, так как это не сушилка
-    this.activeView = 'millygm9517-graph';
+    this.setView('millygm9517-graph');
   }
 
-  // Метод для отображения графиков мельницы ШБМ №3
   showMillycvok130Graph() {
-    this.selectedObjectId = ''; // Сбросьте идентификатор, так как это не сушилка
-    this.activeView = 'millycvok130-graph';
+    this.setView('millycvok130-graph');
   }
 }
