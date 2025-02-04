59998ba02ace762f2e3e24c24c88636dc47b7e5d
Dmitry <melkov.dmitriy40@gmail.com>
2025-02-04 10:39:16 +0500

    add fix control btn

--- src/app/components/control-button/control-button.component.ts
+++ src/app/components/control-button/control-button.component.ts
@@ -1,9 +1,10 @@
 import { CommonModule } from '@angular/common';
 import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { MatIcon } from '@angular/material/icon';
 
 @Component({
   selector: 'app-control-button',
-  imports: [CommonModule],
+  imports: [CommonModule, MatIcon],
   standalone: true,
   templateUrl: './control-button.component.html',
   styleUrls: ['./control-button.component.scss'],
@@ -12,7 +13,14 @@ export class ControlButtonComponent {
   @Input() isActive: boolean = false;
   @Input() isDisabled: boolean = false;
   @Output() onClick = new EventEmitter<Event>();
-  @Input() borderRadius: string = '0px'; // Добавляем свойство для радиуса
+  @Input() borderRadius: string = '0px'; // Свойство для радиуса
+  @Input() iconName: string = 'info '; // Имя материал-иконки
+  @Input() iconHeight: string = '24px'; // Высота иконки
+  @Input() iconWidth: string = '24px'; // Ширина иконки
+  @Input() paddingTop: string = '2px'; // Верхний отступ
+  @Input() paddingBottom: string = '2px'; // Нижний отступ
+  @Input() paddingLeft: string = '5px'; // Левый отступ
+  @Input() paddingRight: string = '5px'; // Правый отступ
 
   get buttonClass() {
     return {
